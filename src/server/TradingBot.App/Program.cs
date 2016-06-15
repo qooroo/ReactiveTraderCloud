b94e552cd657f26169d7953b34726bb2c856458b
Qiming Liu <qooroo@gmail.com>
2016-06-15 15:13:59 +0100

    get app running

--- src/server/TradingBot.App/Program.cs
+++ src/server/TradingBot.App/Program.cs
@@ -19,21 +19,25 @@ namespace TradingBot.App
 
             p.Run().Wait();
 
-            Console.WriteLine("hai");
             Console.ReadLine();
         }
 
         private async Task Run()
         {
-            var config = ServiceConfiguration.FromArgs(new string[] {"prod.config.json"});
+            Console.WriteLine("Running...");
+
+            var config = ServiceConfiguration.FromArgs(new string[] {"config.docker.json"});
 
             IConnected<IBroker> connectedBroker;
             using (var connectionFactory = BrokerConnectionFactory.Create(config.Broker))
             {
+                connectionFactory.Start();
                 connectedBroker = await connectionFactory.GetBrokerStream()
                     .Where(x => x.IsConnected)
                     .Take(1);
             }
+
+            Console.WriteLine("Connected to Broker");
 
             var broker = connectedBroker.Value;
 
@@ -42,23 +46,25 @@ namespace TradingBot.App
                 .Select(x => x.Instance)
                 .Take(1);
 
+            Console.WriteLine($"Found execution service {_executionService}");
+
             broker.SubscribeToTopic<SpotPriceDto>("prices")
-                .CreateBuyOrder("EURUSD", 1.4m, 1000000)
+                .CreateBuyOrder("EURUSD", 1.09393m, 1000000)
                 .Subscribe(x => Buy(x));
 
             broker.SubscribeToTopic<SpotPriceDto>("prices")
-                .CreateSellOrder("EURUSD", 1.5m, 1000000)
+                .CreateSellOrder("EURUSD", 1.09493m, 1000000)
                 .Subscribe(x => Sell(x));
         }
 
         private void Buy(dynamic trade)
         {
-            
+            Console.WriteLine($"Buying {trade}");
         }
 
         private void Sell(dynamic trade)
         {
-            
+            Console.WriteLine($"Selling {trade}");
         }
     }
 }
