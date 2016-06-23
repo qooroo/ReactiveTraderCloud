65c70a30a3b47c1d98f71a3a4a398fa702f14846
Qiming Liu <qooroo@gmail.com>
2016-06-23 13:59:03 +0100

    orders service

---
+++ src/server/Adaptive.ReactiveTrader.Server.Orders/OrdersService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Adaptive.ReactiveTrader.Contract;
+using Adaptive.ReactiveTrader.Messaging;
+using Adaptive.ReactiveTrader.Messaging.Abstraction;
+using Adaptive.ReactiveTrader.Messaging.WAMP;
+using Newtonsoft.Json;
+using WampSharp.V2.Client;
+using WampSharp.V2.Core.Contracts;
+
+namespace OrdersService
+{
+    public class OrdersService : ServiceHostBase
+    {
+        private readonly IWampRealmProxy _realmProxy;
+        private readonly string _executionService;
+        private readonly IBroker _broker;
+
+        public OrdersService(IBroker broker, string type, IWampRealmProxy realmProxy, string executionService) : base(broker, type)
+        {
+            _broker = broker;
+            _realmProxy = realmProxy;
+            _executionService = executionService;
+        }
+
+        public void Initialize()
+        {
+            // Register a handler for calls to placeOrder
+            RegisterCall("placeOrder", PlaceOrder);
+            StartHeartBeat();
+            Console.WriteLine($"Service {InstanceID} up and running");
+        }
+
+        private Task PlaceOrder(IRequestContext ctx, IMessage msg)
+        {
+            Console.WriteLine($"Received Order from {ctx.UserSession.Username}:");
+
+            var trade = JsonConvert.DeserializeObject<ExecuteTradeRequestDto>(Encoding.UTF8.GetString(msg.Payload));
+
+            Console.WriteLine($"{trade.Direction} {trade.CurrencyPair} at {trade.SpotRate}, notional {trade.Notional}");
+
+            _broker.SubscribeToTopic<SpotPriceDto>("prices")
+                .CreateOrder(trade)
+                .Subscribe(ExecuteTrade);
+
+            return Task.FromResult("Order placed");
+        }
+
+        private void ExecuteTrade(ExecuteTradeRequestDto trade)
+        {
+            Console.WriteLine($"Executing order to {trade.Direction} {trade.CurrencyPair} at {trade.SpotRate}, notional {trade.Notional}");
+
+            _realmProxy.RpcCatalog.Invoke
+                (new ConsoleCallback(),
+                    new CallOptions(),
+                    $"{_executionService}.executeTrade",
+                    new object[] { new MessageDto { Payload = trade, ReplyTo = "", Username = "Orders Service" } });
+        }
+    }
+}
---
+++ src/server/Adaptive.ReactiveTrader.Server.Orders/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using WampSharp.V2;
+using Adaptive.ReactiveTrader.Common;
+using Adaptive.ReactiveTrader.Common.Config;
+using Adaptive.ReactiveTrader.Messaging;
+using WampSharp.V2.Client;
+
+namespace OrdersService
+{
+    public class Program
+    {
+        private string _executionService;
+        private IServiceConfiguration _config;
+        private IWampRealmProxy _realmProxy;
+        private IBroker _broker;
+
+        public static void Main(string[] args)
+        {
+            new Program().Run(args);
+
+            Console.ReadLine();
+        }
+
+        private async void Run(string[] args)
+        {
+            _config = ServiceConfiguration.FromArgs(args);
+
+            await ConnectToBroker();
+
+            var serviceHost = new OrdersService(_broker, "orders", _realmProxy, _executionService);
+
+            serviceHost.Initialize();
+        }
+
+        // Note: This is a overly simplified version of the broker connection code. The other services
+        // use a fuller version which reacts to broker/eventstore disconnections and reconnections
+        private async Task ConnectToBroker()
+        {
+            Console.WriteLine("Initializing connection...");
+
+            // 1. Find a broker that is connected
+            IConnected<IBroker> connectedBroker;
+            using (var connectionFactory = BrokerConnectionFactory.Create(_config.Broker))
+            {
+                connectionFactory.Start();
+                connectedBroker = await connectionFactory.GetBrokerStream()
+                    .Where(x => x.IsConnected)
+                    .Take(1);
+            }
+
+            Console.WriteLine("Connected to Broker");
+
+            _broker = connectedBroker.Value;
+
+            // 2. Find an instance of an execution service
+            _executionService = await _broker.SubscribeToTopic<HeartbeatDto>("status")
+                .Where(x => x.Type == "execution")
+                .Select(x => x.Instance)
+                .Take(1);
+
+            Console.WriteLine($"Found execution service {_executionService}");
+
+            // 3. Create a proxy to publish messages to
+            var factory = new DefaultWampChannelFactory();
+            var channel = factory.CreateJsonChannel($"ws://{_config.Broker.Host}:{_config.Broker.Port}/ws", _config.Broker.Realm);
+
+            await channel.Open();
+
+            Console.WriteLine($"Opened channel to {_executionService}");
+
+            _realmProxy = channel.RealmProxy;
+        }
+    }
+}
